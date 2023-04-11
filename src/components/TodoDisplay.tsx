cc28c1fb52d061913ef90fdbab73a46b9f695ba5
Kofi Agyemang <agyemko@gmail.com>
2023-04-11 12:16:18 +0100

    add more styling to make the app pop

--- src/components/TodoDisplay.tsx
+++ src/components/TodoDisplay.tsx
@@ -24,6 +24,7 @@ const TodoDisplay = ({ children, onClick, onChecked }: Props) => {
             <Tr key={item.id}>
               <Td>
                 <Checkbox
+                  outline="solid blue 1px"
                   colorScheme="green"
                   onChange={() => onChecked(item.id)}
                   isChecked={item.completed}
